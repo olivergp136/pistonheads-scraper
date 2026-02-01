c59395109ba2b5d49be908cf7fae4502a9870f21
olivergp136 <oliver@hangar136.com>
2026-02-01 12:42:12 +0000

    Create known_makes.py

---
+++ scraper/known_makes.py
@@ -0,0 +1,21 @@
+# scraper/known_makes.py
+
+FALLBACK_MAKES = [
+    "AC","Adams","AJS","Alfa Romeo","Alpina","Alpine","Alvis","Aprilia","Ariel","Ascari",
+    "Aston Martin","Audi","Austin","Austin Healey","BAC","Bedford","Bentley","Bitter","BMW",
+    "Bond","Bowler","Bristol","BSA","Buell","Bugatti","Buick","Cadillac","Caterham","Chevrolet",
+    "Chrysler","Citroen","Comma","Dacia","Daf","Daihatsu","Daimler","Darrian","Datsun","Davrian",
+    "Dax","De Tomaso","Delorean","Dennis","Dodge","Ducati","Evante","Facel Vega","Farbio","Ferrari",
+    "Fiat","Fisher","Ford","Gardner Douglas","GB Roadster","Gilbern","Ginetta","GMC","Gordon-Keeble",
+    "Grinnell","Gumpert","Harley Davidson","Hillman","Holden","Honda","Hummer","Husaberg","Hyundai",
+    "Indian","Infiniti","Iso Rivolta","Isuzu","Jaguar","JBA Cars","Jeep","Jensen","Kawasaki","Kia",
+    "Kit Cars","Koenigsegg","KTM","Lada","Lagonda","Lamborghini","Lancia","Land Rover","Laverda",
+    "Lexus","Leyland","Locost","Lola","Lotus","Marcos","Marlin","Maserati","Matra","Mazda","McLaren",
+    "Mercedes","Mercury","Messerschmidt","MG","MINI","Mitsubishi","Morgan","Morris","Mosler","Moto Guzzi",
+    "Moto Morini","Motobi","MV Agusta","MZ","Nissan","Noble","Oldsmobile","Opel","Other","Pagani","Panther",
+    "Peugeot","PGO","Piaggio","Polestar","Pontiac","Porsche","Proton","Racing Car","Radical","Range Rover",
+    "Reliant","Renault","Riley","Rolls Royce","Rover","Royal Enfield","Saab","Scott","Seat","Shelby","Silk",
+    "Simca","Singer","Skoda","smart","Spyker","Strathcarron","Subaru","Sunbeam","Suzuki","Sylva","Talbot",
+    "Tatra","Tesla","Toyota","Trabant","TriBSA","Trident","Triton","Triumph","TVR","Ultima","Vanden Plas",
+    "Vauxhall","Velocette","Venturi","Vincent","Volkswagen","Volvo","Westfield","Wolseley","Yamaha","Zenos",
+]
